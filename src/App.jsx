02d69656b1cb3e4d5b8747b993d6e4ae080d9604
Zakariya Elmansouri <zakariyaelmansouri40@exemple.com>
2025-04-23 16:46:29 +0100

    Ton message de commit ici

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+// src/App.jsx
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Sidebar from './components/layout/Sidebar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import FileList from './components/File/FileList';
+import FileUpload from './components/File/FileUpload';
+import UserList from './components/User/UserList';
 
 function App() {
   return (
-    <div>
-      <h1>Bienvenue sur Sharelly!</h1>
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
+      <div style={{ marginLeft: '220px', padding: '20px', width: '100%' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<FileList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/files" element={<FileList />} />
+          <Route path="/upload" element={<FileUpload />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
+      </div>
     </div>
   );
 }
